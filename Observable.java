61d7ee02c3ef11107ce36bf3b35bfd3bb3dc0b05
丁晖 <dinghuidev@163.com>
2017-02-14 15:14:01 +0800

    添加代码
    添加代码

---
+++ Observable.java
@@ -0,0 +1,90 @@
+package org.rxjava;
+
+/**
+ * Created by hinotohui on 17/2/14.
+ */
+public class Observable {
+
+    private OnSubcribe onSubcribe;
+    private Subscriber subscriber;
+
+    public <U,V> Observable lift(final Observable preObservable,
+                                 final Operator<? extends V, ? super U>
+                                         operator){
+        return new Observable(new OnSubcribe<V>() {
+            @Override
+            public void call(Subscriber<? super V> subscriber) {
+                preObservable.onSubcribe.call(operator
+                        .operate(subscriber));
+            }
+        });
+    }
+
+    public <U,V> Observable map(IFunc<U,V> fun){
+        OperatorMap operatorMap = new OperatorMap(fun);
+        Observable observable = lift(this,operatorMap);
+        return observable;
+    }
+
+    public <T> Observable attach(Subscriber<T> subscriber){
+        this.subscriber=subscriber;
+        return this;
+    }
+
+    public <T> void subscribe(Subscriber<T> subscriber){
+        if (onSubcribe!=null)
+            onSubcribe.call(subscriber);
+    }
+
+    public void execute(){
+        if (onSubcribe!=null&&subscriber!=null)
+            onSubcribe.call(subscriber);
+    }
+
+    private Observable(OnSubcribe onSubcribe){
+        this.onSubcribe=onSubcribe;
+    }
+
+    public interface OnSubcribe<T>{
+        void call(Subscriber<? super T> subscriber);
+    }
+
+    public interface Operator<U, V> {
+        Subscriber<? super V> operate(Subscriber<? super U> u);
+    }
+
+    public static <T> Observable create(OnSubcribe<T> onSubcribe){
+        return new Observable(onSubcribe);
+    }
+
+    public static void main(String[] args){
+        Observable.create(new OnSubcribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                String update="update";
+                subscriber.onNext(update);
+            }
+        }).map(new IFunc<String, String>() {
+            @Override
+            public String call(String o) {
+                return o+"123";
+            }
+        }).subscribe(new Subscriber<Object>() {
+            @Override
+            public void onNext(Object o) {
+                System.out.println((String) o);
+                onComplete();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("end");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+        });
+    }
+}
